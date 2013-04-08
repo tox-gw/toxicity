c2410c4279eaab11dce33731daeb4851ad5ca137
Tyler Sy <Tyler@tyler-pc.(none)>
2013-04-07 20:47:33 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toxicity::Application.config.secret_token = '1e8942d788f0a44a754037190c19160d3c21a0e4cc4a7ef8bf81a936f4e2d2fa8ebd01bfce9515a55f86a0a31c70b0c1c75eff4e3a9ba2433e61d3944f2614a5'
